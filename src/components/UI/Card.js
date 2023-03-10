b9ba48e1ca33e4165e928f602bcf9632225250a0
Roushan Singh <Roushankr.singh007@gmail.com>
2023-03-10 20:04:22 +0530

    updated file

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import classes from './Card.module.css'
 
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className={classes.card}>
       {props.children}
